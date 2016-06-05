307290285300526d918e069cb0e9bcf04274c9e7
tomas statter <github@autotelik.co.uk>
2016-06-05 19:41:46 +0100

    install rolify

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,18 +1,14 @@
-module DigitalServicesCore
-  class Role < ActiveRecord::Base
-    self.table_name = "dsc_roles"
 
-    # Users + Roles in own shared DB - external to the main APP
-    establish_connection DigitalServicesCore::User.establish_connection_to_users_db_name
+class Role < ActiveRecord::Base
 
-    has_and_belongs_to_many :users, join_table: :dsc_users_roles
+  has_and_belongs_to_many :users, join_table: :users_roles
 
-    belongs_to :resource, polymorphic: true
+  belongs_to :resource, polymorphic: true
 
-    validates :resource_type,
-              inclusion: { in: Rolify.resource_types },
-              allow_nil: true
+  validates :resource_type,
+            inclusion: { in: Rolify.resource_types },
+            allow_nil: true
 
-    scopify
-  end
+  scopify
 end
+
